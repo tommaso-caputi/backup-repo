5ed7fe8ecf89565dfb0ff888c8347924b3323f20
unknown <tommasocaputi85@gmail.com>
2022-11-23 12:53:44 +0100

    .

--- main.py
+++ main.py
@@ -1,12 +1,23 @@
 from github import Github
 from dotenv import load_dotenv
 import os
+import urllib3
 
 load_dotenv()
-
 ACCESS_TOKEN = os.getenv('TOKEN')
 
 g = Github(ACCESS_TOKEN)
 
+repos = g.get_user().get_repos()
 
-print(g)
+repo = repos[0]
+
+http = urllib3.PoolManager()
+r = http.request('GET', "https://api.github.com/repos/"+g.get_user()+"/"+repo+"/contents/", preload_content=False, headers={'Authorization': "token " + ACCESS_TOKEN})
+with open("/tmp/master.zip", 'wb') as out:
+    while True:
+        data = r.read(64)
+        if not data:
+            break
+        out.write(data)
+r.release_conn()
